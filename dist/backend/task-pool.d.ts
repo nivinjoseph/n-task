60c020753f1674acbf342a6227ad3e08184a267f
nivinjoseph <nivinjoseph@outlook.com>
2021-06-26 13:20:59 -0400

    preparing to publish new version

--- dist/backend/task-pool.d.ts
+++ dist/backend/task-pool.d.ts
@@ -1,13 +1,19 @@
+import "@nivinjoseph/n-ext";
 import { Disposable } from "@nivinjoseph/n-util";
 export declare class TaskPool implements Disposable {
     private readonly _taskWorkerFile;
-    private readonly _callbacks;
-    private readonly _taskWorkers;
     private readonly _count;
-    private readonly _bp;
-    private _isDisposed;
+    private readonly _taskWorkers;
+    private readonly _taskQueue;
+    private _isInitialized;
+    private _disposePromise;
+    private get _isDisposed();
     constructor(taskWorkerFile: string, count?: number);
+    initializeWorkers(initializerMethod?: string, ...initializerParams: any[]): Promise<void>;
     invoke<T>(method: string, ...params: any[]): Promise<T>;
     dispose(): Promise<void>;
-    private initialize;
+    private _createWorkers;
+    private _onAvailable;
+    private _enqueue;
+    private _executeAvailableWork;
 }
--- dist/backend/task-pool.js
+++ dist/backend/task-pool.js
@@ -10,83 +10,155 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.TaskPool = void 0;
+require("@nivinjoseph/n-ext");
 const n_defensive_1 = require("@nivinjoseph/n-defensive");
 const n_util_1 = require("@nivinjoseph/n-util");
 const n_exception_1 = require("@nivinjoseph/n-exception");
 const worker_threads_1 = require("worker_threads");
 class TaskPool {
     constructor(taskWorkerFile, count = 1) {
-        this._callbacks = {};
         this._taskWorkers = new Array();
-        this._isDisposed = false;
+        this._taskQueue = new Array();
+        this._isInitialized = false;
+        // private readonly _bp: BackgroundProcessor;
+        this._disposePromise = null;
         n_defensive_1.given(taskWorkerFile, "taskWorkerFile").ensureHasValue().ensureIsString();
         this._taskWorkerFile = taskWorkerFile;
         n_defensive_1.given(count, "count").ensureHasValue().ensureIsNumber().ensure(t => t > 0);
         this._count = count;
-        this._bp = new n_util_1.BackgroundProcessor((e) => {
-            console.error(e);
-            return Promise.resolve();
-        }, 500, true);
-        this.initialize();
+    }
+    get _isDisposed() { return this._disposePromise != null; }
+    initializeWorkers(initializerMethod, ...initializerParams) {
+        return __awaiter(this, void 0, void 0, function* () {
+            n_defensive_1.given(initializerMethod, "initializerMethod").ensureIsString();
+            n_defensive_1.given(initializerParams, "initializerParams").ensureIsArray().ensureIsArray();
+            n_defensive_1.given(this, "this").ensure(t => !t._isInitialized, "already initialized");
+            if (this._isDisposed)
+                throw new n_exception_1.ObjectDisposedException(this);
+            this._createWorkers();
+            if (initializerMethod != null)
+                yield Promise.all(this._taskWorkers.map(t => t.execute(n_util_1.Uuid.create(), initializerMethod, ...initializerParams)));
+            this._isInitialized = true;
+        });
     }
     invoke(method, ...params) {
         n_defensive_1.given(method, "method").ensureHasValue().ensureIsString();
         n_defensive_1.given(params, "params").ensureHasValue().ensureIsArray();
+        n_defensive_1.given(this, "this").ensure(t => t._isInitialized, "not initialized");
         if (this._isDisposed)
             throw new n_exception_1.ObjectDisposedException(this);
-        return new Promise((resolve, reject) => {
-            const id = n_util_1.Uuid.create();
-            this._callbacks[id] = {
-                resolve,
-                reject
-            };
-            this._bp.processAction(() => __awaiter(this, void 0, void 0, function* () {
-                let freeWorker = this._taskWorkers.find(t => !t.isBusy);
-                while (!freeWorker) {
-                    yield n_util_1.Delay.milliseconds(500);
-                    freeWorker = this._taskWorkers.find(t => !t.isBusy);
-                }
-                freeWorker.isBusy = true;
-                freeWorker.taskWorker.postMessage({
-                    id,
-                    type: method.trim(),
-                    params
-                });
-            }));
-        });
+        return this._enqueue(method, params);
     }
     dispose() {
         return __awaiter(this, void 0, void 0, function* () {
             if (!this._isDisposed) {
-                this._isDisposed = true;
-                yield this._bp.dispose(true);
-                yield Promise.all(this._taskWorkers.map(t => t.taskWorker.terminate()));
+                this._taskQueue.forEach(t => t.deferred.reject("disposed"));
+                this._taskQueue.clear();
+                this._disposePromise = Promise.all(this._taskWorkers.map(t => t.dispose()));
             }
+            return this._disposePromise;
         });
     }
-    initialize() {
+    _createWorkers() {
         n_util_1.Make.loop(() => {
-            const taskWorker = new worker_threads_1.Worker(this._taskWorkerFile);
-            const tpTaskWorker = {
-                taskWorker,
-                isBusy: false
-            };
-            taskWorker.on("message", (data) => {
-                const id = data.id;
-                const error = data.error;
-                const result = data.result;
-                if (this._callbacks[id]) {
-                    if (error != null)
-                        this._callbacks[id].reject(error);
-                    else
-                        this._callbacks[id].resolve(result);
-                    tpTaskWorker.isBusy = false;
-                    delete this._callbacks[id];
-                }
-            });
-            this._taskWorkers.push(tpTaskWorker);
+            const taskWorker = new TaskWorkerInstance(this._taskWorkerFile);
+            taskWorker.initialize(this._onAvailable.bind(this));
+            this._taskWorkers.push();
         }, this._count);
+    }
+    _onAvailable(twi) {
+        n_defensive_1.given(twi, "twi").ensureHasValue().ensureIsObject().ensureIsType(TaskWorkerInstance);
+        this._executeAvailableWork(twi);
+    }
+    _enqueue(method, params) {
+        const taskItem = {
+            id: n_util_1.Uuid.create(),
+            deferred: new n_util_1.Deferred(),
+            method,
+            params
+        };
+        this._taskQueue.unshift(taskItem);
+        this._executeAvailableWork();
+        return taskItem.deferred.promise;
+    }
+    _executeAvailableWork(twi) {
+        if (this._taskQueue.isEmpty)
+            return;
+        const availableWorker = twi !== null && twi !== void 0 ? twi : this._taskWorkers.find(t => !t.isBusy);
+        if (availableWorker == null)
+            return;
+        const work = this._taskQueue.pop();
+        availableWorker
+            .execute(work.id, work.method, ...work.params)
+            .then(t => work.deferred.resolve(t))
+            .catch(e => work.deferred.reject(e));
     }
 }
 exports.TaskPool = TaskPool;
+class TaskWorkerInstance {
+    constructor(taskWorkerFile) {
+        this._availabilityObserver = null;
+        this._disposePromise = null;
+        this._currentTask = null;
+        n_defensive_1.given(taskWorkerFile, "taskWorkerFile").ensureHasValue().ensureIsString();
+        this._id = n_util_1.Uuid.create();
+        this._worker = new worker_threads_1.Worker(taskWorkerFile);
+    }
+    get _isInitialized() { return this._availabilityObserver != null; }
+    get _isDisposed() { return this._disposePromise != null; }
+    get id() { return this._id; }
+    get isBusy() { return this._currentTask != null; }
+    initialize(availabilityCallback) {
+        n_defensive_1.given(availabilityCallback, "availabilityCallback").ensureHasValue().ensureIsFunction();
+        n_defensive_1.given(this, "this").ensure(t => !t._isInitialized, "already initialized");
+        if (this._isDisposed)
+            throw new n_exception_1.ObjectDisposedException(this);
+        this._availabilityObserver = new n_util_1.Observer("available", availabilityCallback);
+        this._worker.on("message", (data) => {
+            const id = data.id;
+            const error = data.error;
+            const result = data.result;
+            if (this._currentTask.id !== id) {
+                this._currentTask.deferred
+                    .reject(new n_exception_1.ApplicationException("Current task id does not match id of task result."));
+            }
+            else {
+                if (error != null)
+                    this._currentTask.deferred.reject(error);
+                else
+                    this._currentTask.deferred.resolve(result);
+            }
+            this._currentTask = null;
+            this._availabilityObserver.notify(this);
+        });
+        return this._availabilityObserver.subscription;
+    }
+    execute(id, method, ...params) {
+        n_defensive_1.given(id, "id").ensureHasValue().ensureIsString();
+        n_defensive_1.given(method, "method").ensureHasValue().ensureIsString();
+        n_defensive_1.given(params, "params").ensureHasValue().ensureIsArray();
+        n_defensive_1.given(this, "this")
+            .ensure(t => t._isInitialized, "worker instance not initialized")
+            .ensure(t => !t.isBusy, "worker instance is busy");
+        if (this._isDisposed)
+            throw new n_exception_1.ObjectDisposedException(this);
+        this._currentTask = {
+            id,
+            deferred: new n_util_1.Deferred()
+        };
+        this._worker.postMessage({
+            id: this._currentTask.id,
+            type: method.trim(),
+            params
+        });
+        return this._currentTask.deferred.promise;
+    }
+    dispose() {
+        if (!this._isDisposed) {
+            this._availabilityObserver.cancel();
+            this._disposePromise = this._worker.terminate();
+        }
+        return this._disposePromise;
+    }
+}
 //# sourceMappingURL=task-pool.js.map
